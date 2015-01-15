5ee1007aa383eeaf3db8b2d6116cd73f3a7c9c40
Nikolaos Anastopoulos <ebababi@ebababi.net>
2015-01-15 12:36:19 +0200

    Rubocop corrections - Lint/AssignmentInCondition

--- lib/foyer/rails.rb
+++ lib/foyer/rails.rb
@@ -5,9 +5,8 @@ module ActionDispatch
     class Mapper
       def authenticate(guard = nil)
         constraint = lambda do |request|
-          if user_id = request.env['rack.session'][Foyer.session_key].try(:[], :id)
-            guard.nil? ? true : guard.call(Foyer.user_finder.call(user_id))
-          end
+          user_id = request.env['rack.session'][Foyer.session_key].try(:[], :id)
+          guard.nil? ? true : guard.call(Foyer.user_finder.call(user_id)) if user_id
         end
 
         constraints(constraint) do
