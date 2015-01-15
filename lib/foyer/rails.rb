0d03f8bf38b42b24f39c4d4fce0e3855774674f5
Nikolaos Anastopoulos <ebababi@ebababi.net>
2015-01-15 12:33:45 +0200

    Rubocop corrections - Style/ClassAndModuleChildren

--- lib/foyer/rails.rb
+++ lib/foyer/rails.rb
@@ -1,16 +1,18 @@
 require 'action_dispatch/routing/mapper'
 
-module ActionDispatch::Routing
-  class Mapper
-    def authenticate(guard=nil)
-      constraint = lambda do |request|
-        if user_id = request.env['rack.session'][Foyer.session_key].try(:[], :id)
-          guard.nil? ? true : guard.call(Foyer.user_finder.call(user_id))
+module ActionDispatch
+  module Routing
+    class Mapper
+      def authenticate(guard=nil)
+        constraint = lambda do |request|
+          if user_id = request.env['rack.session'][Foyer.session_key].try(:[], :id)
+            guard.nil? ? true : guard.call(Foyer.user_finder.call(user_id))
+          end
         end
-      end
 
-      constraints(constraint) do
-        yield
+        constraints(constraint) do
+          yield
+        end
       end
     end
   end
