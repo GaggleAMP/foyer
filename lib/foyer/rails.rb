58df53be848bfefb01b7681a648515812579cef1
Jason Nochlin <hundredwatt@users.noreply.github.com>
2015-08-20 15:46:19 -0400

    Ensure session keys can be accessed with symbols
    The new Rails session serializer returns a hash with string keys

--- lib/foyer/rails.rb
+++ lib/foyer/rails.rb
@@ -5,7 +5,7 @@ module ActionDispatch
     class Mapper
       def authenticate(guard = nil)
         constraint = lambda do |request|
-          user_id = request.env['rack.session'][Foyer.session_key].try(:[], :id)
+          user_id = request.env['rack.session'][Foyer.session_key].try(:with_indifferent_access).try(:[], :id)
           guard.nil? ? true : guard.call(Foyer.user_finder.call(user_id)) if user_id
         end
 
